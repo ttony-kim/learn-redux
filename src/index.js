02894cf392d7ff32062be1d260281bb2a890ad4d
dskim <ektha115@gmail.com>
2022-08-16 09:51:40 +0900

    Couter, Todos

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import reportWebVitals from "./reportWebVitals";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./modules";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
